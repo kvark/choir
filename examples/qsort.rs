78c4d71b5b1793f8af10b3839c982b9de5a2dd21
Dzmitry Malyshau <kvark@fastmail.com>
2022-04-13 21:45:40 -0700

    Integrate profiling, add examples sub-crate (#1)

---
+++ examples/qsort.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut choir = choir::Choir::new();
+    let _worker = choir.add_worker("main");
+    for _ in 0..10000000 {
+        choir.run_task(|| {});
+    }
+    choir.wait_idle();
+}
